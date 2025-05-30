3d4475e37530277eafe68ed28a8bd5496af67e9f
soami.charan <soami.angajala@builder.ai>
2025-05-30 12:46:02 +0530

    [feature] Implemented custom length delimiter format

--- lib/main.rb
+++ lib/main.rb
@@ -19,11 +19,20 @@ def add(numbers)
   # Delimiters
   default_delimiters = [',', '\n']
 
+  # Custom delimiter regex pattern
+  custom_delimiter_pattern = %r{\A//(\[[^\[\]]+\])+\n}
+
   # If delimiter format is provided
   if numbers.start_with?("//")
+    if !custom_delimiter_pattern.match?(numbers)
+      raise ArgumentError, "Invalid delimiter format."
+    end
+
     delimiter_match = numbers.match(/(?<delimiter_format>\/\/(?<delimiter>\D+)\n)?/m)
     if delimiter_match[:delimiter_format] && delimiter_match[:delimiter]
-      default_delimiters << delimiter_match[:delimiter]
+      # Extract delimiter string from inside brackets
+      bracket_delimiters = delimiter_match[:delimiter].scan(/\[(?<delimiter>[^\[\]]+)\]/).flatten
+      default_delimiters += bracket_delimiters
       
       # remove delimiter format
       numbers = numbers[delimiter_match.end(0)..]
@@ -32,17 +41,19 @@ def add(numbers)
     end
   end
   
-  # Check for negetive numbers
-  negetive_checks = numbers.gsub(/(?<=^|\D)(?<num>-\d*\.?\d+)/).map{ Regexp.last_match[:num] }
-  if !negetive_checks.empty?
-    raise ArgumentError, "Negetives not allowed - #{negetive_checks.join(', ')}"
-  end
+  # Escape special character *, [, ], ^, - for regex.
+  default_delimiters = default_delimiters.map{ |delim| delim.gsub(/([\-\^\]\[\*])/, '\\\\\1')  }
   
   # Replace any delimiter to | character as placeholder
   # Split string by | character to get potential list of numbers
-  numbers_format = numbers.gsub(/([#{default_delimiters.join('|')}])/, '|').split("|").map {
+  numbers_format = numbers.gsub(/#{default_delimiters.uniq.join('|')}/, '|').split("|").map {
     |numeric_string| parse_to_number(numeric_string)
   }
+
+  negetive_numbers = numbers_format.filter{ |num| num < 0 }
+  if negetive_numbers.any?
+    raise ArgumentError, "Negetives not allowed - #{negetive_numbers.map(&:to_s).join(', ')}"
+  end
     
   numbers_format.filter{ |num| num < 1000 }.sum
 end
