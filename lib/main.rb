4cefd52842a57c3ae389877126aba5c50e73f66c
soami.charan <soami.angajala@builder.ai>
2025-05-30 08:41:16 +0530

    [feature] Implemented raise exception on negetive numbers

--- lib/main.rb
+++ lib/main.rb
@@ -10,25 +10,6 @@ def parse_to_number(numeric_string)
   end
 end
 
-def is_number(numeric_string)
-  # Check first character if its a number then return true
-  # If it starts with . then look next character and if its number then its a decimal number, return true
-  # If it starts with - then look for next character, if its number then its negetive integer
-  # If it starts with -. then look for third character, if its number then its negetive decimal number.
-  numeric_string[0] =~ /[0-9]/ ||
-  (numeric_string[0] == '.' && numeric_string[1] && numeric_string[1] =~ /[0-9]/) ||
-  (
-    numeric_string[0] == '-' && numeric_string[1] &&
-    (
-      numeric_string[1] =~ /[0-9]/ ||
-      (numeric_string[1] == '.' && numeric_string[2] && numeric_string[2] =~ /[0-9]/)
-    )
-  )
-end
-  
-  
-  
-
 def add(numbers)
   if numbers.empty?
     return 0
@@ -51,35 +32,17 @@ def add(numbers)
     end
   end
   
+  # Check for negetive numbers
+  negetive_checks = numbers.gsub(/(?<=^|\D)(?<num>-\d*\.?\d+)/).map{ Regexp.last_match[:num] }
+  if !negetive_checks.empty?
+    raise ArgumentError, "Negetives not allowed - #{negetive_checks.join(', ')}"
+  end
+  
   # Replace any delimiter to | character as placeholder
   # Split string by | character to get potential list of numbers
   numbers_format = numbers.gsub(/([#{default_delimiters.join('|')}])/, '|').split("|").map {
     |numeric_string| parse_to_number(numeric_string)
   }
     
-  # 0..numbers.size do |idx|
-  #   # Take raw character
-  #   number = "#{numbers[idx].inspect[1..-2]}"
-    
-  #   # If character is a digit OR it's the start of a number with '.' or '-' (when buffer is empty)
-  #   if is_number(numbers[idx..])
-  #     number_buffer += number
-      
-  #   # If delimiter detected then parse number buffer to number and add it to total
-  #   # Reset the number buffer
-  #   elsif delimiters.include?(number)
-  #     total += parse_to_number(number_buffer)
-  #     number_buffer = ""
-      
-  #   # Any other case is regarded as a non numeric digit occured in numeric string.
-  #   else
-  #     raise ArgumentError, "Input string contains non numeric character #{number}."
-  #   end
-  # end
-  
-  # if number_buffer.empty?
-  #   raise ArgumentError,"Input string is invalid as ends with delimiter"
-  # end
   numbers_format.sum
-    
 end
