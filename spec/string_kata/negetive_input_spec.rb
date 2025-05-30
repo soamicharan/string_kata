0a009c1627e84f2300eacaa1cf0117a615d75642
soami.charan <soami.angajala@builder.ai>
2025-05-30 08:39:34 +0530

    [feature][rspec] Added test cases for negetive number scenario

---
+++ spec/string_kata/negetive_input_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require_relative '../../lib/main'
+
+RSpec.describe 'string kata' do
+  it 'adds numbers with no negetive' do
+    expect(add("1\n2,3")).to eq(6)
+  end
+
+  it 'add numbers some negetive numbers' do
+    expect{ add("1\n-2\n3,-5") }.to raise_error(ArgumentError, "Negetives not allowed - -2, -5")
+  end
+  
+  it 'add numbers on hyphen as delimiter' do
+    expect(add("//-\n1-2\n3")).to eq(6)
+  end
+  
+  it 'add numbers on hyphen as delimiter with negetive numbers' do
+    expect{ add("//-\n1-2--3---4") }.to raise_error(ArgumentError, "Negetives not allowed - -3, -4")
+  end
+end
